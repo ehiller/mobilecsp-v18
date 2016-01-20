435b20aff70f1f07092b8478a5cae8fa0aee83ad
Ralph Morelli <ram8647@gmail.com>
2016-01-20 18:29:47 -0500

    Updates modules/quizly.

--- modules/quizly/quizly.py
+++ modules/quizly/quizly.py
@@ -19,8 +19,6 @@
 #    USA
 #
 
-import json
-
 """Quizly exercises for Course Builder.
 
 
@@ -39,9 +37,9 @@ Here is how to install, activate and use this module:
           modules.quizly.quizly.register_module().enable()
     - restart your local development sever or re-deploy your application
     - edit a lesson using visual editor; you should be able to add a new
-      component type "Quizly Exercise"
-    - the component editor should show a list of all exercises available in a
-      dropdown list; there should be over 400 exercises listed here
+      component type "Ram8647: Quizly Exercise"
+    - the component editor should show a list of all available Quizly exercises in a
+      dropdown list; these are from the asset file:  quizzes.json 
     - pick one exercise, save the component configuration
     - add empty exercise definition var activity = []; uncheck 'Activity Listed'
     - save  the lesson
@@ -51,7 +49,7 @@ Here is how to install, activate and use this module:
       EventEntity table with a namespace appropriate for your course
     - this is it!
 
-This work is based on my other project, which brings Khan Academy exercises to
+This work is based on Simikov's khanex project, which brings Khan Academy exercises to
 WordPress. You can learn more about it here:
   http://www.softwaresecretweapons.com/jspwiki/khan-exercises
 
@@ -59,7 +57,7 @@ Here are the things I found difficult to do while completing this integration:
     - if a unit is marked completed in a progress tracking system, and author
       adds new unit - the progress indicator is now wrong and must be recomputed
       to account for a new added unit
-    - why don't we circles next to the lessons of the left nav. of the unit?
+    - why don't we see circles next to the lessons of the left nav. of the unit?
     - how does a tag/module know what unit/lesson is being shown now? we need to
       pass some kind of course_context into tag/module... how much of view model
       do we expose to the tag/module? can a tag instance introspect what is
@@ -80,6 +78,8 @@ Good luck!
 
 __author__ = 'Pavel Simakov (pavel@vokamis.com), Ralph Morelli (ram8647@gmail.com)'
 
+import json
+import logging
 import cgi
 import os
 import urllib2
@@ -97,6 +97,14 @@ from models.counters import PerfCounter
 from models import custom_modules
 from models import models
 from models import transforms
+from google.appengine.api import users
+
+#  We override these classes
+from controllers.lessons import EventsRESTHandler
+from controllers.lessons import TAGS_THAT_TRIGGER_COMPONENT_COMPLETION
+
+from models.progress import UnitLessonCompletionTracker
+from models.progress import TRACKABLE_COMPONENTS 
 
 
 ATTEMPT_COUNT = PerfCounter(
@@ -130,37 +138,98 @@ class QuizlyExerciseTag(tags.BaseTag):
     def vendor(cls):
         return 'ram8647'
 
-    def render(self, node, unused_handler):
-        quizname = node.attrib.get('quizname')
-        print 'QUIZLY    quizname = ' + quizname
-        preamble = node.attrib.get('preamble')
+    def render(self, node, handler):
 
-        # GCB doesn't like spaces. This replaces with %20
+        #  Get the context and the Quizly question data
+        student = handler.student
+        unitid = handler.unit_id
+        lessonid = handler.lesson_id
+
+        quizname = node.attrib.get('quizname')
+        instanceid = node.attrib.get('instanceid')
+        preamble = node.attrib.get('preamble')
         preamble = urllib2.quote(preamble)
 
-        height = node.attrib.get('height') or '495'
-        width = node.attrib.get('width') or '795'
+        id_iconholder = 'icon-holder-' + quizname    #  Div where icon goes in HTML doc
+
+        logging.info('RAM: *** QUIZLY render: student=%s unit=%s lesson=%s instance=%s quiz=%s', student.email,unitid, lessonid, instanceid, quizname)
+
+        # Creates a record of quiz on window.quizlies.
+        script = ''
+        script += '<script>'
+        script += 'if (!window.quizlies) {window.quizlies={};}'
+        script += 'var quiz = {};'
+        script += 'quiz.name="' + quizname + '";'
+        script += 'quiz.id="' + instanceid + '";'
+        script += 'window.quizlies["' + quizname + '"]= quiz;'
+#        script += 'console.log("instanceid="' + instanceid + '"");'  # syntax error here
+        script += '</script>'
+
+        # View attributes        
+        height = node.attrib.get('height') or '595'
+        width = node.attrib.get('width') or '755'
         hasanswerbox = node.attrib.get('hasanswerbox') or 'false'
         isrepeatable = node.attrib.get('isrepeatable') or 'false'
         hashints = node.attrib.get('hints') or 'true'
-        script = '<script src="assets/lib/quizly/activity-quizme.js"></script>'
-        script += '<script>'
-        script += 'window.quiz={};'
-        script += 'window.quiz.name ="' + quizname + '";'
-        script += 'window.quiz.quizType ="' + quizname + '";'
-        script += '</script>'
+
+        #  Source of the iframe that will be loaded for this quiz
         src = 'assets/lib/quizly/gcb-index.html?backpack=hidden&selector=hidden&quizname=' + quizname
-        print 'QUIZLY    preamble = ' + preamble
         if preamble:
-          print 'QUIZLY    Adding preamble = ' + preamble
           src += '&heading=' + preamble
         src += '&hints=' + hashints
         src += '&repeatable=' + isrepeatable
-        onclick = 'checkQuizmeAnswer(\'' + quizname + '\')'
-        onclicknewquestion = 'giveNewQuestion(\'' + quizname + '\')'
+
+        # Has this lesson already been completed?
+        progress_tracker = handler.get_course().get_progress_tracker()
+        student_progress = progress_tracker.get_or_create_progress(student)
+        completed = progress_tracker.is_component_completed(student_progress, unitid, lessonid, instanceid)
+
+        # Handler for the checkAnswer button. Quizly puts the quizName and result
+        #  on window.parent.quizlies and this script reads it from there.
+        # NOTE:  gcbAudit() is in activity-generic.js
+        script += '<script> function checkAnswer(){ '
+        script +=   'var quizName = window.quizlies["quizname"];'
+        script +=   'var instanceid = window.quizlies[quizName].id;'
+        script +=   'var result = window.quizlies[quizName].result;'
+        script +=   'console.log("RAM (quizly.py):  That solution was " + result);'
+#       This immediately updates the blue dot but that's not how it works for other quizzes.
+#        script +=   'if (result==true) {'
+#        script +=      'var id_iconholder = "icon-holder-" + quizName;'
+#        script +=      'var ih = document.getElementById(id_iconholder);'
+#        script +=      'ih.innerHTML=\'1 point <img src="assets/lib/completed.png" />\''
+#        script +=   '}'
+        script +=   'if (gcbCanPostTagEvents) {'
+        script +=      'console.log("RAM (quizly.py): POSTing to server");'
+        script +=      'console.log("RAM (quizly.py): instanceid=" + instanceid);'
+        script +=      'var auditDict = {'
+        script +=         '\'instanceid\': instanceid,'
+        script +=         '\'answer\': result,'
+        script +=         '\'score\': (result) ? 1 : 0,'
+        script +=         '\'type\': "quizly",'
+        script +=      '};'
+        script +=      'gcbAudit(auditDict, "tag-quizly", true);'
+        script +=   '}'
+        script += '}'
+        script += '</script>';
+
         returnStr = ''
         returnStr += script
-        returnStr += '<iframe src=' + src + ' width="' + width + '" height="' + height + '"></iframe>'
+
+        #  Add the box-surrounded iframe that will hold the quiz. Blockly will be a frame w/in this frame.
+        returnStr += '<div style="border: 1px solid black; margin: 5px; padding: 5px;">'
+        returnStr += '<div id="' + id_iconholder + '" class="gcb-progress-icon-holder gcb-pull-right">'
+        progress_img = '1 point <img src="assets/lib/completed.png" />' if completed else '1 point <img src="assets/lib/not_started.png" />' 
+        returnStr += progress_img
+#        returnStr += '1 point <img src="assets/lib/not_started.png" />'
+        returnStr += '</div>'
+        returnStr += '<iframe style= "border: 0px; margin: 1px; padding: 1px;" src=' + src + ' width="' + width + '" height="' + height + '"></iframe>'
+        returnStr += '</div>'
+
+#  Commented out -- this would add a separate GCB "Check Answer" button.  Currently "Check Answer" is within Quizly frame.
+#         returnStr += '<div class="qt-check-answer">'
+#         returnStr += '<button onclick="checkAnswer()" class="gcb-button qt-check-answer-button">Check Answer</button>'
+#         returnStr += '</div>'
+
         return tags.html_string_to_element_tree(returnStr)
 
     def get_schema(self, unused_handler):
@@ -189,12 +258,12 @@ class QuizlyExerciseTag(tags.BaseTag):
                 optional=True,
                 extra_schema_dict_values={'value': 'false'},
                 description='Input Text Field'))
-        reg.add_property(
-            schema_fields.SchemaField(
-                'isrepeatable', 'New Question Button', 'boolean',
-                optional=True,
-                extra_schema_dict_values={'value': 'false'},
-                description='New Question Button'))
+#         reg.add_property(
+#             schema_fields.SchemaField(
+#                 'isrepeatable', 'New Question Button', 'boolean',
+#                 optional=True,
+#                 extra_schema_dict_values={'value': 'false'},
+#                 description='New Question Button'))
         reg.add_property(
             schema_fields.SchemaField(
                 'hints', 'Hints Button', 'boolean',
@@ -205,118 +274,137 @@ class QuizlyExerciseTag(tags.BaseTag):
             schema_fields.SchemaField(
                 'height', 'Height', 'string',
                 optional=True,
-                extra_schema_dict_values={'value': '495'},
+                extra_schema_dict_values={'value': '595'},
                 description=('Height of the iframe')))
         reg.add_property(
             schema_fields.SchemaField(
                 'width', 'Width', 'string',
                 optional=True,
-                extra_schema_dict_values={'value': '995'},
+                extra_schema_dict_values={'value': '755'},
                 description=('Width of the iframe')))
         return reg
 
 
-class QuizlyExerciseRenderer(utils.BaseHandler):
-    """A handler that renders Quizly Exercise."""
+class QuizlyEventsRESTHandler(EventsRESTHandler):
+    """Overrides the default EventsRESTHandler to add tag-quizly"""
 
-    def _render_indirect(self, slug):
-        parts = slug.split(':')
-        if len(parts) != 2:
-            raise Exception(
-                'Error processing request. Expected \'ity_ef_slug\' in a form '
-                'of \'protocol:identifier\'.')
+    TAGS_THAT_TRIGGER_COMPONENT_COMPLETION.append('tag-quizly')
 
-        if 'static' != parts[0]:
-            raise Exception('Bad protocol.')
+class QuizlyUnitLessonCompletionTracker(UnitLessonCompletionTracker):
+    """Overrides the default  to add quizly to TRACKABLE list"""
 
-        zip_file = zipfile.ZipFile(ZIP_FILE)
-        html_file = zip_file.open(EXERCISE_BASE + parts[1] + '.html')
-        self.response.write(html_file.read())
+    TRACKABLE_COMPONENTS.append('quizly')
 
-    def _record_student_submission(self, data):
-        """Record data in a specific course namespace."""
-        # get student
-        student = self.personalize_page_and_get_enrolled()
-        if not student:
-            return False
 
-        # record submission
-        models.EventEntity.record(
-            'module-quizly.exercise-submit', self.get_user(), data)
+# Currently unused -- copied from khanex. Don't really understand
+# how it works.
+# class QuizlyExerciseRenderer(utils.BaseHandler):
+#     """A handler that renders Quizly Exercise."""
 
-        # update progress
-        unit_id, lesson_id = self._get_unit_lesson_from(data)
-        self.get_course().get_progress_tracker().put_activity_accessed(
-            student, unit_id, lesson_id)
+#     def _render_indirect(self, slug):
+#         parts = slug.split(':')
+#         if len(parts) != 2:
+#             raise Exception(
+#                 'Error processing request. Expected \'ity_ef_slug\' in a form '
+#                 'of \'protocol:identifier\'.')
 
-        return True
+#         if 'static' != parts[0]:
+#             raise Exception('Bad protocol.')
 
-    def _get_unit_lesson_from(self, data):
-        """Extract unit and lesson id from exercise data submission."""
+#         zip_file = zipfile.ZipFile(ZIP_FILE)
+#         html_file = zip_file.open(EXERCISE_BASE + parts[1] + '.html')
+#         self.response.write(html_file.read())
 
-        # we need to figure out unit and lesson id for the exercise;
-        # we currently have no direct way of doing it, so we have to do it
-        # indirectly == ugly...; an exercise captures a page URL where it was
-        # embedded; we can parse that URL out and find all the interesting
-        # parts from the query string
+#     def _record_student_submission(self, data):
+#         """Record data in a specific course namespace."""
+#         logging.info('RAM: ******************* QUIZLY record_student_submission')
 
-        unit_id = 0
-        lesson_id = 0
-        json = transforms.loads(data)
-        if json:
-            location = json.get('location')
-            if location:
-                location = urllib2.unquote(location)
-                params_map = urlparse.parse_qs(location)
-                ity_ef_origin = params_map.get('ity_ef_origin')
-                if ity_ef_origin:
-                    ity_ef_origin = ity_ef_origin[0]
-                    origin_path = urlparse.urlparse(ity_ef_origin)
-                    if origin_path.query:
-                        query = urlparse.parse_qs(origin_path.query)
-                        unit_id = self._int_list_to_int(query.get('unit'))
-                        lesson_id = self._int_list_to_int(query.get('lesson'))
+#         # get student
+#         student = self.personalize_page_and_get_enrolled()
+#         if not student:
+#             return False
 
-                        # when we are on the first lesson of a unit, leson_id is
-                        # not present :(; look it up
-                        if not lesson_id:
-                            lessons = self.get_course().get_lessons(unit_id)
-                            if lessons:
-                                lesson_id = lessons[0].lesson_id
+#         # record submission
+#         models.EventEntity.record(
+#             'module-quizly.exercise-submit', self.get_user(), data)
 
-        return unit_id, lesson_id
+#         # update progress
+#         unit_id, lesson_id = self._get_unit_lesson_from(data)
+#         self.get_course().get_progress_tracker().put_activity_accessed(
+#             student, unit_id, lesson_id)
 
-    def _int_list_to_int(self, list):
-        if list:
-            return int(list[0])
-        return 0
+#         return True
 
-    def post(self):
-        """Handle POST, i.e. 'Check Answer' button is pressed."""
-        data = self.request.get('ity_ef_audit')
-        if self._record_student_submission(data):
-            ATTEMPT_COUNT.inc()
-            self.response.write('{}')  # we must return valid JSON on success
-            return
+#     def _get_unit_lesson_from(self, data):
+#         """Extract unit and lesson id from exercise data submission."""
 
-        self.error(404)
+#         logging.info('RAM: ******************* QUIZLY get_unit_lesson_from')
 
-    def get(self):
-        """Handle GET."""
-        rule = self.request.get('ity_ef_rule')
-        slug = self.request.get('ity_ef_slug')
+#         # we need to figure out unit and lesson id for the exercise;
+#         # we currently have no direct way of doing it, so we have to do it
+#         # indirectly == ugly...; an exercise captures a page URL where it was
+#         # embedded; we can parse that URL out and find all the interesting
+#         # parts from the query string
 
-        # render raw
-        if rule == 'raw':
-            self.response.write(EXERCISE_HTML_PAGE_RAW)
-            return
+#         unit_id = 0
+#         lesson_id = 0
+#         json = transforms.loads(data)
+#         if json:
+#             location = json.get('location')
+#             if location:
+#                 location = urllib2.unquote(location)
+#                 params_map = urlparse.parse_qs(location)
+#                 ity_ef_origin = params_map.get('ity_ef_origin')
+#                 if ity_ef_origin:
+#                     ity_ef_origin = ity_ef_origin[0]
+#                     origin_path = urlparse.urlparse(ity_ef_origin)
+#                     if origin_path.query:
+#                         query = urlparse.parse_qs(origin_path.query)
+#                         unit_id = self._int_list_to_int(query.get('unit'))
+#                         lesson_id = self._int_list_to_int(query.get('lesson'))
 
-        # render indirect
-        if slug and _allowed(slug):
-            self._render_indirect(slug)
-            return
+#                         # when we are on the first lesson of a unit, leson_id is
+#                         # not present :(; look it up
+#                         if not lesson_id:
+#                             lessons = self.get_course().get_lessons(unit_id)
+#                             if lessons:
+#                                 lesson_id = lessons[0].lesson_id
 
-        self.error(404)
+#         return unit_id, lesson_id
+
+#     def _int_list_to_int(self, list):
+#         if list:
+#             return int(list[0])
+#         return 0
+
+#     def post(self):
+#         """Handle POST, i.e. 'Check Answer' button is pressed."""
+
+#         logging.info('RAM: ******************* QUIZLY post()')
+#         data = self.request.get('ity_ef_audit')
+#         if self._record_student_submission(data):
+#             ATTEMPT_COUNT.inc()
+#             self.response.write('{}')  # we must return valid JSON on success
+#             return
+
+#         self.error(404)
+
+#     def get(self):
+#         """Handle GET."""
+#         rule = self.request.get('ity_ef_rule')
+#         slug = self.request.get('ity_ef_slug')
+
+#         # render raw
+#         if rule == 'raw':
+#             self.response.write(EXERCISE_HTML_PAGE_RAW)
+#             return
+
+#         # render indirect
+#         if slug and _allowed(slug):
+#             self._render_indirect(slug)
+#             return
+
+#         self.error(404)
 
 
 custom_module = None
@@ -329,16 +417,17 @@ def register_module():
     tags.Registry.add_tag_binding('quizly', QuizlyExerciseTag)
 
     # register handler
-    zip_handler = (
-        '/quizly-exercises', sites.make_zip_handler(ZIP_FILE))
-    render_handler = (
-        '/quizly-exercises/quizly-exercises/indirect/', QuizlyExerciseRenderer)
+#     zip_handler = (
+#         '/quizly-exercises', sites.make_zip_handler(ZIP_FILE))
+#     render_handler = (
+#         '/quizly-exercises/quizly-exercises/indirect/', QuizlyExerciseRenderer)
 
     # register module
     global custom_module
     custom_module = custom_modules.Module(
         'Quizly Exercise',
-        'A set of pages for delivering Quizly Exercises via '
+        'A set of exercises for delivering Quizly Exercises via '
         'Course Builder.',
-        [], [render_handler, zip_handler])
+        [], [])
+#        [], [render_handler, zip_handler])
     return custom_module
